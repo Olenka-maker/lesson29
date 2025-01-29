488264014da2f8bf5e6a2730fc9657ad02fed410
Olenka-maker <olenkachmil14@gmail.com>
2025-01-29 12:48:31 +0200

    first commit

---
+++ wor29.js
@@ -0,0 +1,33 @@
+function showPage(page) {
+  const content = document.getElementById("content");
+  content.innerHTML = `<h2>${page.charAt(0).toUpperCase() + page.slice(1)} Page</h2><div class='list' id='list'></div>`;
+  
+  if (page === "users") {
+      fetchData("https://jsonplaceholder.typicode.com/users");
+  } else if (page === "posts") {
+      fetchData("https://jsonplaceholder.typicode.com/posts");
+  } else if (page === "comments") {
+      fetchData("https://jsonplaceholder.typicode.com/comments");
+  }
+}
+
+//Завдання 5  Візьміть будь яку лекцію по ООП, додайте застосування call, apply, bind
+//call
+function hello() {
+  console.log('Hello i am ' + this.name);
+}
+
+let user = { name: 'Olena' };
+hello.call(user)
+
+//apply
+function intr(city, age) {
+  console.log('My name is ' + this.name + ', my age is ' + age + ', my place is ' + city);
+  
+}
+
+intr.apply(user, ['Lviv', 15])
+
+//bind
+const nHello = hello.bind(user);
+nHello()
